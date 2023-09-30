15eec1d42eff24e7a5871ca9e4d5b212cc3740dd
Mayank <butterly095@gmail.com>
2023-09-30 13:27:44 +0530

    Reduced lines of code

--- minesweeper.py
+++ minesweeper.py
@@ -26,156 +26,113 @@ print(
 
 
 def revealBombs(board, location):
-    # location at centre--------------------------------------------
-    if location > 11 and location < 90:
-            # to right 
+    def right():
+        # to right
         if board[location + 1] in bombs:
             board[location] = " X"
         else:
             board[location] = "  "
-            # to bottom
+
+    def left():
+        # to left
+        if board[location - 2] in bombs:
+            board[location - 2] = " X"
+        else:
+            board[location - 2] = "  "
+
+    def top():
+        # to top
+        if board[location - 9] in bombs:
+            board[location - 11] = " X"
+        else:
+            board[location - 11] = "  "
+
+    def down():
+        # to bottom
         if board[location + 9] in bombs:
             board[location + 9] = " X"
         else:
             board[location + 9] = "  "
-            # to top
-        if board[location-9] in bombs:
-            board[location-11] = " X"
-        else:
-            board[location-11] = "  "
-            # to left
-        if board[location -2] in bombs:
-            board[location-2] = " X"
-        else:
-            board[location-2] = "  "
-        
+
+    # location at centre--------------------------------------------
+    if location > 11 and location < 90:
+        # to right
+        right()
+        # to bottom
+        down()
+        # to top
+        top()
+        # to left
+        left()
+
         return
- 
+
     # location left wall----------------------------------
-    if location%10 ==1:
+    elif location % 10 == 1:
         if location == 1:
-            if board[location + 1] in bombs:
-                board[location] = " X"
-            else:
-                board[location] = "  "
-                
-            if board[location + 9] in bombs:
-                board[location + 9] = " X"
-            else:
-                board[location + 9] = "  "  
+            # to right
+            right()
+
+            # to bottom
+            down()
         elif location == 91:
-            # to right 
-            if board[location + 1] in bombs:
-                board[location] = " X"
-            else:
-                board[location] = "  "
+            # to right
+            right()
             # to top
-            if board[location-9] in bombs:
-                board[location-11] = " X"
-            else:
-                board[location-11] = "  "
+            top()
         else:
-                # to right 
-            if board[location + 1] in bombs:
-                board[location] = " X"
-            else:
-                board[location] = "  "
-                # to bottom
-            if board[location + 9] in bombs:
-                board[location + 9] = " X"
-            else:
-                board[location + 9] = "  "
-                # to top
-            if board[location-9] in bombs:
-                board[location-11] = " X"
-            else:
-                board[location-11] = "  "
+            # to right
+            right()
+            # to bottom
+            down()
+            # to top
+            top()
         return
 
     # location top wall-------------------------------------
-    if location//10 <= 1:
-            # to right 
-        if board[location + 1] in bombs:
-            board[location] = " X"
-        else:
-            board[location] = "  "
-            # to bottom
-        if board[location + 9] in bombs:
-            board[location + 9] = " X"
-        else:
-            board[location + 9] = "  "
-            
-            # to left
-        if board[location -2] in bombs:
-            board[location-2] = " X"
-        else:
-            board[location-2] = "  " 
+    elif location // 10 <= 1:
+        # to right
+        right()
+        # to bottom
+        down()
+
+        # to left
+        left()
         
-
-    # location right wall ----------------------------
-    if location%10 == 0:
-        if location == 10:
-            # to left
-            if board[location -2] in bombs:
-                board[location-2] = " X"
-            else:
-                board[location-2] = "  " 
-            # to bottom
-            if board[location + 9] in bombs:
-                board[location + 9] = " X"
-            else:
-                board[location + 9] = "  "  
-        elif location == 100:
-            # to left
-            if board[location -2] in bombs:
-                board[location-2] = " X"
-            else:
-                board[location-2] = "  "             
-
-            # to top
-            if board[location-9] in bombs:
-                board[location-11] = " X"
-            else:
-                board[location-11] = "  "
-        else:
-                # to right 
-            if board[location + 1] in bombs:
-                board[location] = " X"
-            else:
-                board[location] = "  "
-                # to bottom
-            if board[location + 9] in bombs:
-                board[location + 9] = " X"
-            else:
-                board[location + 9] = "  "
-                # to top
-            if board[location-9] in bombs:
-                board[location-11] = " X"
-            else:
-                board[location-11] = "  "
         return
 
+    # location right wall ----------------------------
+    elif location % 10 == 0:
+        if location == 10:
+            # to left
+            left()
+            # to bottom
+            down()
+        elif location == 100:
+            # to left
+            left()
+
+            # to top
+            top()
+        else:
+            # to right
+            right()
+            # to bottom
+            down()
+            # to top
+            top()
+        return
 
     # location bottom wall
-    if location//91 >= 1:
-            # to right 
-        if board[location + 1] in bombs:
-            board[location] = " X"
-        else:
-            board[location] = "  "
-            # to top
-        if board[location-9] in bombs:
-            board[location-11] = " X"
-        else:
-            board[location-11] = "  "
-            
-            # to left
-        if board[location -2] in bombs:
-            board[location-2] = " X"
-        else:
-            board[location-2] = "  "   
-    
-    
+    else:
+        # to right
+        right()
+        # to top
+        top()
+        # to left
+        left()
+        
+        return
 
 
 # bomb_density = input('Choose difficulty:\nEasy(10bombs)\nMedium(20 Bombs)\nImpossible(40 Bombs)')
@@ -186,18 +143,17 @@ bombs = choices(board, k=20)
 
 chances = 3
 
-while chances>0:
+while chances > 0:
     location = int(input("\nEnter your choice: "))
     if location in bombs:
         chances -= 1
         board[location - 1] = " X"
         printBoard(board)
         print("Oppsie, you stepped on a bomb.\nChances left: ", chances)
-        
-        
+
     else:
         board[location - 1] = "  "
         revealBombs(board, location)
         printBoard(board)
 
-print('Game over!')
+print("Game over!")
